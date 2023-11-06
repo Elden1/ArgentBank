17729be981bfb7bf1bb90704eaba6dc6ce53d70e
Elden1 <t.betsch@hotmail.com>
2023-11-06 16:59:50 +0100

    modification img

--- frontend/src/Containers/Header/index.jsx
+++ frontend/src/Containers/Header/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './assets/style.scss';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import bankLogo from './assets/argentBankLogo.png';
+import bankLogo from './assets/argentBankLogo.webp';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import SignOutButton from '../../Components/LogOut';
 
